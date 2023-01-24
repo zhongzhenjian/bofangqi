235f35e08f5beed7724fc7b9c6396713f71fec9e
zhongzhenjian <731422879@qq.com>
2023-01-24 20:33:52 +0800

    完善

--- web/application/api/controller/Sreach.php
+++ web/application/api/controller/Sreach.php
@@ -30,7 +30,33 @@ class Sreach extends Api
             $this->error('ＭＳＴＡ　ＧＥＴ');
         }
         $req = $request->get();
-        $res = Dvideo::with('labels')->where('title', 'like', '%' . $req['title'] . '%')->page($req['current'], $req['every'])->select();
+
+        //查询条件
+        $where = [];
+
+        //标题
+        if(isset($req['title']) && null != $req['title'] && "" != $req['title'])
+            $where['title'] = [ 'LIKE', "%{$req['title']}%"];
+        //视频分类
+        if(isset($req['class']) && null != $req['class'] && "" != $req['class'])
+            $where['class'] = ['=', $req['class']];
+        //是否有码
+        if(isset($req['mosaic']) && null != $req['mosaic'] && "" != $req['mosaic'])
+            $where['mosaic'] = ['=', $req['mosaic']];
+        //地区
+        if(isset($req['area']) && null != $req['area'] && "" != $req['area'])
+            $where['area'] = ['=', $req['area']];
+        //视频长短
+        if(isset($req['duration']) && null != $req['duration'] && "" != $req['duration'])
+            $where['duration'] = ['=', $req['duration']];
+
+        //排序
+        $order = [];
+        if(isset($req['sort']) && null != $req['sort'] && "" != $req['sort'])
+            $order[] = $req['sort'] . ' desc';
+
+        //$res = Dvideo::with('labels')->where('title', 'like', '%' . $req['title'] . '%')->page($req['current'], $req['every'])->select();
+        $res = Dvideo::with('labels')->where($where)->page($req['current'], $req['every'])->order($order)->select();
         $res ?$res = $res->toArray():'';
         $this->success('视频搜索结果',$res,200);
     }
